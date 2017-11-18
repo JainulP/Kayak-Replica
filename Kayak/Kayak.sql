1d3a85b14887f4214c3c2121823a28993f3ab441
JainulP <jainul.patel@sjsu.edu>
2017-11-18 12:20:46 -0800

    schema updated

--- Kayak/Kayak.sql
+++ Kayak/Kayak.sql
@@ -81,100 +81,6 @@ LOCK TABLES `creditcards` WRITE;
 /*!40000 ALTER TABLE `creditcards` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `hotel`
---
-
-DROP TABLE IF EXISTS `hotel`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `hotel` (
-  `HotelId` int(11) NOT NULL AUTO_INCREMENT,
-  `HotelName` varchar(500) NOT NULL,
-  `Location` varchar(500) NOT NULL,
-  `ReviewScore` decimal(10,0) DEFAULT NULL,
-  `Phone` varchar(45) DEFAULT NULL,
-  `StreetAddress` varchar(255) DEFAULT NULL,
-  `State` varchar(255) DEFAULT NULL,
-  `Longitude` varchar(45) DEFAULT NULL,
-  `Latitude` varchar(45) DEFAULT NULL,
-  `ZipCode` varchar(45) DEFAULT NULL,
-  `Stars` varchar(45) DEFAULT NULL,
-  `Description` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`HotelId`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `hotel`
---
-
-LOCK TABLES `hotel` WRITE;
-/*!40000 ALTER TABLE `hotel` DISABLE KEYS */;
-INSERT INTO `hotel` VALUES (1,'Fairmont San Jose','San Jose, CA',8,'66778897','170 South Market Street','CA',NULL,NULL,'95113','4',NULL),(2,'Hotel De Anza','San Jose, CA',8,'33445533','233 West Santa Clara Street','CA',NULL,NULL,'95113','4',NULL),(3,'Wyndham Garden San Jose – Silicon Valley','San Jose, CA',8,'55667722','399 Silicon Valley Boulevard','CA',NULL,NULL,'95138','3',NULL),(4,'Row NYC','New York, NY',7,'4433243','700 8th Avenue','NY',NULL,NULL,'10036','4',NULL),(5,'Sofitel New York','New York, NY',9,'343546','45 West 44TH Street','NY',NULL,NULL,'10036','3',NULL);
-/*!40000 ALTER TABLE `hotel` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `hotelbooking`
---
-
-DROP TABLE IF EXISTS `hotelbooking`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `hotelbooking` (
-  `BillingId` int(11) NOT NULL,
-  `GuestId` int(11) NOT NULL,
-  `GuestName` varchar(45) NOT NULL,
-  `RoomId` int(11) NOT NULL,
-  `CheckInDate` datetime NOT NULL,
-  `CheckOutDate` datetime NOT NULL,
-  PRIMARY KEY (`BillingId`),
-  KEY `RoomId_idx` (`RoomId`),
-  CONSTRAINT `RoomId` FOREIGN KEY (`RoomId`) REFERENCES `rooms` (`RoomId`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `hotelbooking`
---
-
-LOCK TABLES `hotelbooking` WRITE;
-/*!40000 ALTER TABLE `hotelbooking` DISABLE KEYS */;
-INSERT INTO `hotelbooking` VALUES (1,1,'Jainul',1,'2017-12-01 00:00:00','2017-12-04 00:00:00'),(2,1,'Jainul',3,'2017-12-06 00:00:00','2017-12-09 00:00:00'),(3,1,'Jainul',5,'2017-12-15 00:00:00','2017-12-25 00:00:00'),(4,1,'Jainul',7,'2018-01-15 00:00:00','2018-01-17 00:00:00'),(5,1,'Jainul',9,'2018-02-05 00:00:00','2018-02-07 00:00:00'),(6,1,'Jainul',2,'2018-12-15 00:00:00','2017-12-25 00:00:00');
-/*!40000 ALTER TABLE `hotelbooking` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `rooms`
---
-
-DROP TABLE IF EXISTS `rooms`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `rooms` (
-  `RoomId` int(11) NOT NULL AUTO_INCREMENT,
-  `HotelId` int(11) DEFAULT NULL,
-  `RoomType` varchar(500) NOT NULL,
-  `Price` int(10) NOT NULL,
-  `Description` varchar(1000) DEFAULT NULL,
-  `MaxGuests` varchar(45) DEFAULT NULL,
-  `Amenities` varchar(1000) DEFAULT NULL,
-  PRIMARY KEY (`RoomId`),
-  KEY `HotelId_idx` (`HotelId`),
-  CONSTRAINT `HotelId` FOREIGN KEY (`HotelId`) REFERENCES `hotel` (`HotelId`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `rooms`
---
-
-LOCK TABLES `rooms` WRITE;
-/*!40000 ALTER TABLE `rooms` DISABLE KEYS */;
-INSERT INTO `rooms` VALUES (1,1,'Delux Room',200,NULL,'3','Shower, Bath, Television'),(2,1,'Standard Room',100,NULL,'3','Shower, Bath, Television'),(3,2,'Delux Room',250,NULL,'3','Shower, Bath, Television'),(4,2,'Standard Room',200,NULL,'3','Shower, Bath, Television'),(5,3,'Delux Room',110,NULL,'3','Shower, Bath, Television'),(6,3,'Standard Room',100,NULL,'3','Shower, Bath, Television'),(7,4,'Delux Room',300,NULL,'3','Shower, Bath, Television'),(8,4,'Standard Room',280,NULL,'3','Shower, Bath, Television'),(9,5,'Delux Room',350,NULL,'3','Shower, Bath, Television'),(10,5,'Standard Room',300,NULL,'3','Shower, Bath, Television'),(11,4,'Delux Room',300,NULL,'3','Shower, Bath, Television');
-/*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -213,6 +119,78 @@ LOCK TABLES `user` WRITE;
 INSERT INTO `user` VALUES (1,'Jainul','Patel','733','San Jose','CA','95112','123123123','jainul.patel@sjsu.edu',NULL,0,NULL,NULL,NULL,NULL,'123123');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+
+
+--
+-- Table structure for table `hotel`
+--
+
+DROP TABLE IF EXISTS `hotel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hotel` (
+  `HotelId` int(11) NOT NULL AUTO_INCREMENT,
+  `HotelName` varchar(500) NOT NULL,
+  `Location` varchar(500) NOT NULL,
+  `ReviewScore` decimal(10,0) DEFAULT NULL,
+  `Phone` varchar(45) DEFAULT NULL,
+  `StreetAddress` varchar(255) DEFAULT NULL,
+  `State` varchar(255) DEFAULT NULL,
+  `Longitude` varchar(45) DEFAULT NULL,
+  `Latitude` varchar(45) DEFAULT NULL,
+  `ZipCode` varchar(45) DEFAULT NULL,
+  `Stars` varchar(45) DEFAULT NULL,
+  `Description` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`HotelId`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hotel`
+--
+
+LOCK TABLES `hotel` WRITE;
+/*!40000 ALTER TABLE `hotel` DISABLE KEYS */;
+INSERT INTO `hotel` VALUES (1,'Fairmont San Jose','San Jose, CA',8,'66778897','170 South Market Street','CA',NULL,NULL,'95113','4',NULL),(2,'Hotel De Anza','San Jose, CA',8,'33445533','233 West Santa Clara Street','CA',NULL,NULL,'95113','4',NULL),(3,'Wyndham Garden San Jose – Silicon Valley','San Jose, CA',8,'55667722','399 Silicon Valley Boulevard','CA',NULL,NULL,'95138','3',NULL),(4,'Row NYC','New York, NY',7,'4433243','700 8th Avenue','NY',NULL,NULL,'10036','4',NULL),(5,'Sofitel New York','New York, NY',9,'343546','45 West 44TH Street','NY',NULL,NULL,'10036','3',NULL);
+/*!40000 ALTER TABLE `hotel` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hotelavailability`
+--
+
+DROP TABLE IF EXISTS `hotelavailability`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hotelavailability` (
+  `HotelId` int(11) DEFAULT NULL,
+  `Date` datetime DEFAULT NULL,
+  `DeluxRoomCount` int(11) DEFAULT NULL,
+  `StandardRoomCount` int(11) DEFAULT NULL,
+  `KingRoomCount` int(11) DEFAULT NULL,
+  `QueenRoomCount` int(11) DEFAULT NULL,
+  `DoubleRoomCount` int(11) DEFAULT NULL,
+  `DeluxRoomPrice` decimal(10,0) DEFAULT NULL,
+  `StandardRoomPrice` decimal(10,0) DEFAULT NULL,
+  `KingRoomPrice` decimal(10,0) DEFAULT NULL,
+  `QueenRoomPrice` decimal(10,0) DEFAULT NULL,
+  `DoubleRoomPrice` decimal(10,0) DEFAULT NULL,
+  `Id` int(50) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hotelavailability`
+--
+
+LOCK TABLES `hotelavailability` WRITE;
+/*!40000 ALTER TABLE `hotelavailability` DISABLE KEYS */;
+INSERT INTO `hotelavailability` VALUES (1,NULL,50,40,20,30,40,300,400,500,350,450,1),(4,NULL,55,45,25,35,45,300,400,500,350,450,2),(2,NULL,20,20,40,30,20,200,200,220,210,210,3),(3,NULL,22,33,44,34,45,300,300,300,300,300,4),(5,NULL,50,60,50,20,25,400,300,350,380,390,5),(4,'2017-11-22 00:00:00',2,0,0,0,0,300,400,500,350,450,6),(4,'2017-11-23 00:00:00',5,5,5,NULL,NULL,300,400,500,350,450,7);
+/*!40000 ALTER TABLE `hotelavailability` ENABLE KEYS */;
+UNLOCK TABLES;
+
 
 --
 -- Dumping routines for database 'kayak'
