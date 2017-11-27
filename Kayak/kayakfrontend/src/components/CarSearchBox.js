757f7214bda7e99a9e7d89fa1f5acae2e8e4c903
sriharshav1 <sriharsha.vanga@sjsu.edu>
2017-11-27 12:38:37 -0800

    Space Issue fix

--- Kayak/kayakfrontend/src/components/CarSearchBox.js
+++ Kayak/kayakfrontend/src/components/CarSearchBox.js
@@ -1,33 +1,13 @@
 import { Route, withRouter,BrowserRouter } from 'react-router-dom';
 import '../App.css';
 import React, { Component } from 'react';
-var carToStyle = {
- position: "relative",
-    top: "-40px",
-    left: "24px"
-  
-};
 
-var places = [
-      "San Jose",
-      "San Fransisco",
-      "New York",
-      "Dallas",
-      "Nevada",
-      "Milpitas",
-      "Colonnade",
-      "Stanford",
-      "Newark",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell"
-    ];
+
 class CarSearchBox extends Component {
     constructor(props) {
             super(props);
         }
-     //componentDidMount() {
+     componentDidMount() {
       /*   debugger;
          var date_input=$('input[name="date"]'); //our date input has the name "date"
 		var container='#aaa'
@@ -38,15 +18,8 @@ class CarSearchBox extends Component {
 			todayHighlight: true,
 			autoclose: true,
 		})*/
-  //  }
-          componentDidMount() {
-          var options = '';
-
-  for(var i = 0; i < places.length; i++)
-    options += '<option value="'+places[i]+'" />';
-
-  document.getElementById('placeList').innerHTML = options;
     }
+         
     
     
 
@@ -64,7 +37,7 @@ class CarSearchBox extends Component {
 }
 
         updateTextInput(txtid,spanid,textboxid) {
-        
+            debugger;
             var tempVal=parseInt(document.getElementById(spanid).value);
             var daytext;
             var result;
@@ -118,48 +91,47 @@ class CarSearchBox extends Component {
 <div className = "row">
 
     <div className = "col-sm-4 col-xs-4 hotelFields">
-<input type = "text" className = "form-control" list ="placeList" id = "flightTo"/>
-                              <datalist id="placeList"></datalist>
-< / div>
+<input type = "text" className = "form-control" id = "flightTo"/>
+</div>
 
 <div className = "col-sm-2 col-xs-2 hotelFields" id = "aaa">
-<input className = "form-control datetimepicker" id = "datePicker1" name = "date"  placeholder = "MM/DD/YYYY   HH" type = "date"  / >
+<input className = "form-control datetimepicker" id = "datePicker1" name = "date"  placeholder = "MM/DD/YYYY   HH" type = "date" />
 
                              
      
-< / div>
+</div>
                               <div className = "col-sm-1 col-xs-1 hotelFields">
-<input type = "text" className = "form-control"  id = "CarToTime"/>
-                               <i className = "glyphicon glyphicon-time calendariconTo" style={carToStyle} name = "date" onClick={()=>this.popUpDisplay("div_change_qty1")}>< / i>
+<input type = "text" className = "form-control" id = "CarToTime"/>
+                               <i className = "glyphicon glyphicon-time calendariconTo"name = "date" onClick={()=>this.popUpDisplay("div_change_qty1")}></i>
 <div id = 'div_change_qty1' className="row" name = 'div_change_qty' >
 <span className="spanClose" onClick={()=>this.popUpClose("div_change_qty1")}>X</span>
 <input type="range" id="rangeId1" name="rangeInput" min="0" max="24" className="slider" onChange={()=>this.updateTextInput('textInput1','rangeId1','CarToTime')}/>
     
 <input type="text" id="textInput1" className="carPopupTxt" value=""/>
      
-< / div>
-< / div>
+</div>
+</div>
 <div className = "col-sm-2 col-xs-2 hotelFields">
-<input className = "form-control datepicker" id = "datePicker2" name = "date" placeholder = "MM/DD/YYYY    HH" type = "date" / >
-< / div>
+<input className = "form-control datepicker" id = "datePicker2" name = "date" placeholder = "MM/DD/YYYY    HH" type = "date"/>
+</div>
   <div className = "col-sm-1 col-xs-1 hotelFields">
 <input type = "text" className = "form-control" id = "CarFromTime"/>
-    <i className = "glyphicon glyphicon-time calendariconFrom" name = "date" style={carToStyle} onClick={()=>this.popUpDisplay("div_change_qty2")}>< / i>
+    <i className = "glyphicon glyphicon-time calendariconFrom"name = "date" onClick={()=>this.popUpDisplay("div_change_qty2")}></i>
 <div id = 'div_change_qty2' name = 'div_change_qty' >
     <span className="spanClose" onClick={()=>this.popUpClose("div_change_qty2")}>X</span>
 <input type="range" id="rangeId2" name="rangeInput" min="0" max="24" className="slider" onChange={()=>this.updateTextInput('textInput2','rangeId2','CarFromTime')}/>
 <input type="text" id="textInput2" className="carPopupTxt" value=""/>
-< / div>
-< / div>
+</div>
+</div>
 
 <div className = "col-sm-1 col-xs-1 hotelFields">
 <button type = "button" className = "btn btn-warning form-control buttonField " onClick={this.props.clickSearchevent}>
-<span className = "glyphicon glyphicon-search">< / span>
-< / button>
-< / div>
-< / div>
-< / div>
-< / div>
+<span className = "glyphicon glyphicon-search"></span>
+</button>
+</div>
+</div>
+</div>
+</div>
    
                  );
 }
