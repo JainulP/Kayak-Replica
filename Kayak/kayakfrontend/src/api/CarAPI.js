1469305af6d1ba57d5583265c35d702f14173bda
manasacsy <32827245+manasacsy@users.noreply.github.com>
2017-11-28 15:31:27 -0800

    car api call port changed

--- Kayak/kayakfrontend/src/api/CarAPI.js
+++ Kayak/kayakfrontend/src/api/CarAPI.js
@@ -1,4 +1,4 @@
-const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:5000'
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:3001'
 
 const headers = {
     'Accept': 'application/json'
