f3edc8964310b091887fbff470d4008e3251b3d2
sriharshav1 <sriharsha.vanga@sjsu.edu>
2017-11-28 15:57:45 -0800

    Payment page modifications

--- Kayak/kayakfrontend/src/components/Payments.js
+++ Kayak/kayakfrontend/src/components/Payments.js
@@ -8,6 +8,31 @@ var divStyle = {
   marginLeft: "220px"
   
 };
+var travellerSpanStyle = {
+
+    cursor: "pointer",
+    color:"blue"
+
+};
+var imgStyle = {
+  width: "25px",
+    height:"25px",
+    cursor:"pointer"
+    
+  
+};
+var deleteStyle={
+    float: "right",
+fontSize: "x-small",
+color: "grey",
+cursor: "pointer"
+}
+var dropdownStyle={
+     
+}
+var buttonStyle = {
+    width: "65px"
+};
 var headStyle = {
 
   marginLeft: "400px"
@@ -15,7 +40,7 @@ var headStyle = {
 };
 var containerStyle = {
 
-  height: "100px"
+  height: "110px"
   
 };
 var BookingResults;
@@ -24,12 +49,14 @@ class Payments extends Component {
     constructor(props){
         super(props);
          this.state ={BookingResults : [{
+             "id":"1",
              "CardName":"visa",
         "PersonName":"Sri Harsha",
         "CardNumber":"8880-8880-8880-8880",
          "cvv":"202",
              "expiry":"08/19"
     },{
+        "id":"2",
          "CardName":"visa",
         "PersonName":"Sri Harsha",
         "CardNumber":"8880-8880-8880-8880",
@@ -40,10 +67,32 @@ class Payments extends Component {
     ]
             }
     }
+     bookingactivitySave(data){
+        debugger;
+    }
+      showbookingactivity(){
+          debugger;
+        
+          var x = document.getElementById("bookingactivity");
+   
+        x.style.display = "block";
     
+       //$('#bookingactivity').show("slow");
+      debugger;
+}
+    bookingactivityclose(){
+          //$('#bookingactivity').hide("slow");
+        var x = document.getElementById("bookingactivity");
+   
+        x.style.display = "none";
+       debugger;
+}
+    travellerDelete(data){
+        debugger;
+    }
       showbookingactivity(para){
           debugger;
-         alert(para);
+        
           //var x = document.getElementById("aa").previousSibling.innerHTML;
         /*  alert( showbookingactivity.caller.arguments[0].target.id);*/
           var x = document.getElementById("bookingactivity");
@@ -68,7 +117,15 @@ class Payments extends Component {
       var idval='1';
       debugger;
      this.state.BookingResults.map(function(lis,index) {
-      
+         var image;
+      if(lis.CardName=="visa")
+          {
+              image="VISA.png"
+          }
+         if(lis.CardName=="mastercard")
+          {
+              image="MasterCard.png"
+          }
        
                 BookingDetailList.push(
                     <div className="w3-container" style={divStyle}>
@@ -76,25 +133,57 @@ class Payments extends Component {
 
   <div className="w3-card-4" >
     <header className="w3-container  w3-blue">
-      <h4>{lis.CardName}--{lis.CardNumber} </h4>
-            <h4>{lis.PersonName}-{lis.cvv}-{lis.expiry}</h4>        
+      <h4><img src={image} style={imgStyle}/>   {lis.CardNumber} </h4>
+                  
     </header>
 
     <div className="w3-container"style={containerStyle}>
-      <p><br/><br/></p>
+              <h6>Person name:{lis.PersonName}</h6>
+                    <h6>CVV Number:{lis.cvv}</h6>
+                    <h6>Expiry(MM/YY):{lis.expiry}</h6>
+                         
+       <a href="#" onClick={()=>this.travellerDelete(lis)} style={deleteStyle}>Delete</a>
     </div>
 
   </div>
+                     <div id="bookingactivity">
+<div id="bookingactivitycontent">
+<h4><b>Add Card</b></h4>
+<h5 className="TravellerContent" id="text1"><span style={dropdownStyle} > CardType : </span>
+                      <select name="cards" style={dropdownStyle}>
+    <option value="visa">visa</option>
+    <option value="mastercard">mastercard</option>
+
+  </select></h5>
+<h5 className="TravellerContent" id="text2"><input type="text" className="form-control" placeholder="Card Number" id="lNameUsr"/></h5>
+<h5 className="TravellerContent" id="text3"><input type="text" className="form-control" placeholder="Name on card" id="emailUsr"/></h5>
+<h5 className="TravellerContent" id="text3"><input type="text" className="form-control" placeholder="CVV" id="contactInfoUsr"/></h5>
+                    <h5 className="TravellerContent" id="text3"><input type="text" className="form-control" placeholder="Expiry(MM/YY)" id="contactInfoUsr"/></h5>
+
+   <br/>
+    <div className="row">
+        <div className="col-sm-6">
+            <button type="button" className="btn btn-warning col-sm-4 bookingContent"  style={buttonStyle} id="popupsave" onClick={()=>this.bookingactivitySave()} value="Save">Save</button>
+        </div>
+        <div className="col-sm-6">
+    <button type="button" className="btn btn-warning col-sm-4 bookingContent" style={buttonStyle}  id="popupclose" onClick={()=>this.bookingactivityclose()} value="Close">Close</button>
+        </div>
+
+    </div>
 </div>
+                                   </div>
+</div>
+                    
 )
-           });
+           },this);
         
          
     return ( 
         <div>
         <h4 style={headStyle}>Stored Cards</h4>
         {BookingDetailList}
-        <br/>
+         <br/><br/>
+        <a className = "travellerTripTag" style={travellerSpanStyle} onClick={() => this.showbookingactivity()}>Add Cards</a>
         
                 </div>
 
