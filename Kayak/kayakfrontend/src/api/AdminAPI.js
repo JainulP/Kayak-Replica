c618c99ed36f0b70cf76f2671bb88529d6805e39
manasacsy <32827245+manasacsy@users.noreply.github.com>
2017-12-03 23:09:21 -0800

    commit issues

---
+++ Kayak/kayakfrontend/src/api/AdminAPI.js
@@ -0,0 +1,44 @@
+var dateTime = require('node-datetime');
+var dt = dateTime.create();
+dt.format('m-d-Y H:M:S');
+
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:3001'
+
+const headers = {
+    'Accept': 'application/json'
+};
+
+export const getAllUsers = () =>
+    fetch(`${api}/getAllUsers`, {
+        method: 'GET',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        credentials:'include',
+    })
+        .then(res => {
+            return res.json();
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+
+
+export const editUserInfo = (payload) =>
+    fetch(`${api}/users/userinfo`, {
+        method: 'POST',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        credentials:'include',
+        body: JSON.stringify(payload)})
+        .then(res => {
+            return res.json();
+        })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
